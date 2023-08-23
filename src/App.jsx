3118708b0f08c39eb271c5a98267ea0c6bfd9fb2
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-08-23 09:42:14 +0300

    homePage + router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './style/App.css';
 import './style/reset.css';
+import HomePage from './pages/HomePage';
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <div className='container'>
+      <h1>HomePage</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo ipsam
+        eius tenetur omnis, quis voluptate.
+      </p>
+    </div>
+  );
+}
